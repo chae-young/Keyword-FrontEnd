cf60e84d4b06f6e86e8d95e6bccb3e2128e2a32f
chae-young <cycy8527@gmail.com>
2023-12-27 14:50:58 +0900

    Merge 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import HomePage from './pages/HomePage';
+
 import SearchPage from './pages/SearchPage';
 import ChatPage from './pages/ChatPage';
 import MyPage from './pages/MyPage';
 import InnerCon from './components/common/InnerCon';
+import HomePage from './pages/HomePage';
+import ChatDetail from './pages/ChatPage/ChatDetail';
 
 interface AppProps {
   children: React.ReactNode;
@@ -20,6 +22,7 @@ const App = ({ children }: AppProps) => (
           <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/chat" element={<ChatPage />} />
+          <Route path="/chat/:id" element={<ChatDetail />} />
           <Route path="/mypage" element={<MyPage />} />
           {/* 404 처리 */}
           {/* <Route path="*" element={<NotFound />}></Route> */}
