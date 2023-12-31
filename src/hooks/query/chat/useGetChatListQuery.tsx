0544e427597795f65b13898c1a117d897e1ccecc
chae-young <cycy8527@gmail.com>
2023-12-31 21:43:14 +0900

    Rename: mock파일이름 handler로 변경

--- src/hooks/query/chat/useGetChatListQuery.tsx
+++ src/hooks/query/chat/useGetChatListQuery.tsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { BASE_URL } from '@/apis';
 import { ChatListDataType } from '@/types/chatting/chatDataType';
 
 const fetchAPI = async (page: number): Promise<ChatListDataType[]> => {
   try {
-    const list = await axios.get(`/chats?size=10&page=${page}`);
+    const list = await axios.get(`${BASE_URL}/chats?size=10&page=${page}`);
     return list.data;
   } catch (error) {
     console.log(error);
