bc4bbd8ab92e14243d3315634c510bff32341d10
Pinn <lsol1130@naver.com>
2024-01-01 23:55:10 +0900

    ScheduleForm

---
+++ src/components/CreateTodo/CreateTodoBody/index.tsx
@@ -0,0 +1,98 @@
+
+import React, { useState } from 'react';
+import Input from '@/components/common/Input';
+import Select from '@/components/common/Select';
+
+interface ScheduleFormProps {
+  onScheduleAdd: (schedule: Schedule) => void;
+}
+
+interface Schedule {
+  title: string;
+  date: string;
+  time: string;
+  alarm: string;
+  address: string;
+  friends: string[];
+}
+
+const ScheduleForm: React.FC<ScheduleFormProps> = ({ onScheduleAdd }) => {
+  const [schedule, setSchedule] = useState<Schedule>({
+    title: '',
+    date: '',
+    time: '',
+    alarm: '',
+    address: '',
+    friends: [],
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setSchedule((prevSchedule) => ({
+      ...prevSchedule,
+      [name]: value,
+    }));
+  };
+
+  
+
+  const handleFriendsChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedFriends = Array.from(e.target.selectedOptions, (option) => option.value);
+    setSchedule((prevSchedule) => ({
+      ...prevSchedule,
+      friends: selectedFriends,
+    }));
+  };
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onScheduleAdd(schedule);
+    // 폼 제출 후 상태 초기화 또는 다른 작업 수행
+    setSchedule({
+      title: '',
+      date: '',
+      time: '',
+      alarm: '',
+      address: '',
+      friends: [],
+    });
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <div>
+        <label>일정 제목</label>
+        <Input type="text" value={schedule.title} handleChangeInput={handleInputChange} />
+      </div>
+      <div>
+        <label>날짜</label>
+        <Input type="date" value={schedule.date} handleChangeInput={handleInputChange} />
+      </div>
+      <div>
+        <label>시간</label>
+        <Input type="time" value={schedule.time} handleChangeInput={handleInputChange}/>
+      </div>
+      <div>
+        <label>알람</label>
+        {/* <Select options={[]} value ={schedule.alarm} handleChangeSelect={handleInputChange}/> */}
+      </div>
+      <div>
+        <label>주소</label>
+        <Input type="text" value={schedule.address} handleChangeInput={handleInputChange}/>
+      </div>
+      <div>
+        <label>친구 선택하기</label>
+        <Select multiple name="friends" value={schedule} onChange={handleFriendsChange}>
+          <option value="친구1">친구1</option>
+          <option value="친구2">친구2</option>
+          <option value="친구3">친구3</option>
+        </Select>
+      </div>
+      <div>
+        <button type="submit">등록하기</button>
+      </div>
+    </form>
+  );
+};
+
+export default ScheduleForm;
---
+++ src/components/common/Input/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface InputProps {
+
+  type: 'text' | 'password' | 'email' | 'number' | 'date' | 'time';
+  width?: string;
+  value: string;
+  placeholder?: string;
+  maxLength?: number;
+  minLength?: number;
+  handleChangeInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({
+  type,
+  width,
+  handleChangeInput,
+  value,
+  placeholder,
+  maxLength,
+  minLength
+}: InputProps) => (
+  <input
+    className={`bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 ${
+      width || 'w-full'
+    }`}
+    placeholder={placeholder}
+    type={type}
+    value={value}
+    minLength={minLength}
+    maxLength={maxLength}
+    onChange={handleChangeInput}
+  />
+);
+
+export default Input;
