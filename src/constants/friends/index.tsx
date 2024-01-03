5b147645521139cfe406a87a88a532fbd5a388ed
chae-young <cycy8527@gmail.com>
2024-01-03 15:08:05 +0900

    Refactor: constans 폴더 생성후 분리!

---
+++ src/constants/friends/index.tsx
@@ -0,0 +1,8 @@
+import {
+  IsFriendRequestType,
+  IsFriendType
+} from '@/types/friend/friendsDataType';
+
+// 친구 상태
+export const IS_FRIEND: IsFriendType = 'FRIEND';
+export const IS_FRIEND_REQUEST: IsFriendRequestType = 'REQUESTING';
