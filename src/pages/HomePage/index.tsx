e27c42a247d33c15fa95555b205cdd8a6e8eb72a
chae-young <cycy8527@gmail.com>
2024-01-04 22:16:24 +0900

    Remove: input 제거

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import InnerCon from '@/components/common/InnerCon';
-import Input from '@/components/common/Input';
 
 interface HomePageProps {}
 
-const HomePage = ({}: HomePageProps) => (
-  <>
-    홈입니다
-    <Input type="text" />
-  </>
-);
+const HomePage = ({}: HomePageProps) => <>홈입니다</>;
 
 export default HomePage;
