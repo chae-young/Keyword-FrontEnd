7a52de181f974f61a9d3a960eebae9631b121b4b
chae-young <cycy8527@gmail.com>
2024-01-05 16:31:06 +0900

    #10 Design: Nav UI & 라우팅 완료

---
+++ src/components/common/Nav/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { AiFillHome } from 'react-icons/ai';
+import { RiSearchLine } from 'react-icons/ri';
+import { IoChatboxEllipses } from 'react-icons/io5';
+import { FaUserAlt } from 'react-icons/fa';
+
+const navList = [
+  {
+    id: 1,
+    title: '홈',
+    path: '/',
+    icon: <AiFillHome className="text-xl mb-1" />
+  },
+  {
+    id: 2,
+    title: '검색',
+    path: '/search',
+    icon: <RiSearchLine className="text-xl mb-1" />
+  },
+  {
+    id: 3,
+    title: '채팅',
+    path: '/chat',
+    icon: <IoChatboxEllipses className="text-xl mb-1" />
+  },
+  {
+    id: 4,
+    title: '마이페이지',
+    path: '/myPage',
+    icon: <FaUserAlt className="text-xl mb-1" />
+  }
+];
+
+const Nav = () => {
+  const commonStyle = 'w-14 flex flex-col items-center text-body3';
+
+  return (
+    <nav className="fixed left-0 bottom-0 right-0 m-auto max-w-default bg-white h-14">
+      <div className="flex items-center justify-around h-full">
+        {navList.map(nav => (
+          <NavLink
+            key={nav.id}
+            to={nav.path}
+            className={({ isActive }) =>
+              isActive ? `${commonStyle} text-bk` : `${commonStyle} text-gray3`
+            }
+          >
+            {nav.icon}
+            {nav.title}
+          </NavLink>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import Nav from '@/components/common/Nav';
 
-const HomePage = ({}: HomePageProps) => <>홈입니다</>;
+const HomePage = ({}: HomePageProps) => (
+  <>
+    홈입니다
+    <Nav />
+  </>
+);
 
 export default HomePage;
