9e768f2e7d4c27ad5adfc65895c66fecbb8c1e4b
Pinn <lsol1130@naver.com>
2024-01-07 03:22:12 +0900

    Feat/CreateSchedule

--- src/pages/todo/CreateTodoPage/index.tsx
+++ src/pages/todo/CreateTodoPage/index.tsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import TopTitle from '@/components/common/TopTitle';
-import CalendarComponent from '@/components/common/Calendar';
-import SelectTime from '@/components/common/Select/SelectOption';
 import WideButton from '@/components/common/Button/WideButton';
+import SelectDate from '@/components/common/Date&TimePicker/Date';
+import TimePicker from '@/components/common/Date&TimePicker/Time';
 
 const CreateTodoPage = () => (
   <>
     <TopTitle title="일정 생성" />
     <form className="pt-0">
-      <div className="pb-4">
+      <div className="pb-4 pt-3">
         <p className="pb-3">일정 제목</p>
         <input
           className="bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 w-full"
           type="text"
         />
       </div>
-      <div className="pt-4">
+      <div className="pb-4 pt-3">
         <p className="pb-3">날짜</p>
-        <CalendarComponent />
+        <SelectDate />
       </div>
-      <div className="pt-4">
+      <div className="pb-4 pt-3">
         <p className="pb-3">시간</p>
-        <SelectTime />
+        <TimePicker />
       </div>
-      <div className="pt-4">
+      <div className="pb-4 pt-3">
+        <p className="pb-3">장소</p>
+      </div>
+      <div className="pb-4 pt-3">
         <p className="pb-3">알림 설정</p>
         <input
           className="bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 w-full"
           type="text"
         />
       </div>
-      <div className="pt-4">
+      <div className="pb-4 pt-3">
         <p className="pb-3">친구선택</p>
         <input
           className="bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 w-full"
