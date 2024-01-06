479db2c8ad1247b2763c28d1ce3fca233aebfec3
Pinn <lsol1130@naver.com>
2024-01-06 18:09:24 +0900

    Feat/일정 생성

--- src/pages/CreateTodoPage/index.tsx
+++ src/pages/CreateTodoPage/index.tsx
@@ -1,19 +1,11 @@
-import React from "react";
-import TopTitle from "@/components/common/TopTitle";
-import ScheduleForm from "@/components/CreateTodo/CreateTodoBody";
-import Schedule from "@/components/CreateTodo/CreateTodoBody";
+import React from 'react';
+import TopTitle from '@/components/common/TopTitle';
+import InnerCon from '@/components/common/InnerCon';
 
+const CreateTodoPage = () => (
+  <InnerCon>
+    <TopTitle title="일정 생성" />
+  </InnerCon>
+);
 
-const CreateTodo = () => {
-
-  return (
-    <>
-      <TopTitle title="일정 생성" />
-      {/* <ScheduleForm onScheduleAdd ={(_schedule: Schedule)=> Schedule} /> */}
-    </>
-  )
-};
-
-
-export default CreateTodo;
-
+export default CreateTodoPage;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import InnerCon from '@/components/common/InnerCon';
+import TopTitle from '@/components/common/TopTitle';
+import WideButton from '@/components/common/Button/WideButton';
+import CalendarComponent from '@/components/common/Calendar';
+import SelectTime from '@/components/common/Select/SelectOption';
 
 const HomePage = () => (
-  <div>
-    안녕하세요
-  </div>
+  <InnerCon>
+    <TopTitle title="일정 생성" />
+    <form className="pt-0">
+      <div className="pb-4">
+        <p className="pb-3">일정 제목</p>
+        <input
+          className="bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 w-full"
+          type="text"
+        />
+      </div>
+      <div className="pt-4">
+        <p className="pb-3">날짜</p>
+        <CalendarComponent />
+      </div>
+      <div className="pt-4">
+        <p className="pb-3">시간</p>
+        <SelectTime />
+      </div>
+      <div className="pt-4">
+        <p className="pb-3">알림 설정</p>
+        <input
+          className="bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 w-full"
+          type="text"
+        />
+      </div>
+      <div className="pt-4">
+        <p className="pb-3">친구선택</p>
+        <input
+          className="bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 w-full"
+          type="text"
+        />
+      </div>
+      <div>
+        <WideButton type="submit" text="일정 등록" status={false} />
+      </div>
+    </form>
+  </InnerCon>
 );
-
 export default HomePage;
