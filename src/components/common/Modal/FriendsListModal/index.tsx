929536e2e007e005ee98714e97c5ac181ef71601
Pinn <lsol1130@naver.com>
2024-01-09 23:22:55 +0900

    Feat/select-friends
    친구목록 중 선택 체크박스
    선택 인원 버튼 반환

---
+++ src/components/common/Modal/FriendsListModal/index.tsx
@@ -0,0 +1,70 @@
+import React, { useCallback, useState } from 'react';
+import { friendsList } from '@/mocks/data';
+
+interface CheckedState {
+  [key: number]: boolean;
+}
+
+const FriendListModal: React.FC = () => {
+  const [checkedState, setCheckedState] = useState<CheckedState>(
+    friendsList.reduce((state, friend) => {
+      state[friend.memberId] = false;
+      return state;
+    }, {})
+  );
+
+  const toggleCheckbox = (memberId: number) => {
+    setCheckedState(prevState => ({
+      ...prevState,
+      [memberId]: !prevState[memberId]
+    }));
+  };
+
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const checkedList = friendsList.filter(
+        friend => checkedState[friend.memberId]
+      );
+      console.log('checkedList:', checkedList);
+    },
+    [checkedState]
+  );
+
+  return (
+    <form onSubmit={onSubmit} className="w-full h-80">
+      <ul>
+        {friendsList.map(friend => (
+          <li key={friend.memberId}>
+            <div className="form-control">
+              <label className="cursor-pointer label">
+                <div className="avatar">
+                  <div className="w-24 rounded-full">
+                    <img src={friend.profileImageUrl} alt={friend.name} />
+                  </div>
+                </div>
+                <span className="label-text">{friend.name}</span>
+                <input
+                  type="checkbox"
+                  checked={checkedState[friend.memberId]}
+                  className="checkbox checkbox-warning"
+                  onChange={() => toggleCheckbox(friend.memberId)}
+                />
+              </label>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <button
+          type="submit"
+          className="btn fixed flex-end mr-3 mb-3 bottom-0 right-0"
+        >
+          {`${Object.values(checkedState).filter(Boolean).length}명 선택`}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default FriendListModal;
--- src/pages/todo/CreateTodoPage/index.tsx
+++ src/pages/todo/CreateTodoPage/index.tsx
@@ -1,52 +1,76 @@
 import React from 'react';
+import { IoPersonAddOutline } from 'react-icons/io5';
 import TopTitle from '@/components/common/TopTitle';
 import WideButton from '@/components/common/Button/WideButton';
 import SelectDate from '@/components/common/Date&TimePicker/Date';
 import TimePicker from '@/components/common/Date&TimePicker/Time';
 import Addr from '@/components/common/Map';
 import SelectRemind from '@/components/common/Select/SelectOption';
-import SelectFriendsButton from '@/components/common/Button/SelectFriendsButton';
 import Modal from '@/components/common/Modal';
+import useModalState from '@/hooks/recoil/useModalState';
+import FriendListModal from '@/components/common/Modal/FriendsListModal';
 
-const CreateTodoPage = () => (
-  <>
-    <TopTitle title="일정 생성" />
-    <form className="pt-0">
-      <div className="pb-4 pt-3">
-        <p className="pb-3">일정 제목</p>
-        <input
-          className="bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 w-full"
-          type="text"
-        />
-      </div>
-      <div className="pb-4 pt-3">
-        <p className="pb-3">날짜</p>
-        <SelectDate />
-      </div>
-      <div className="pb-4 pt-3">
-        <p className="pb-3">시간</p>
-        <TimePicker />
-      </div>
-      <div className="pb-4 pt-3">
-        <p className="pb-3">장소</p>
-        <Addr />
-      </div>
-      <div className="pb-4 pt-3">
-        <p className="pb-3">알림 설정</p>
-        <SelectRemind />
-      </div>
-      <div className="pb-4 pt-3">
-        <p className="pb-3">친구선택</p>
-        <div>
-          <SelectFriendsButton text="친구를 선택해보세요" status={false} />
-          <Modal />
+const CreateTodoPage = () => {
+  const { openModal } = useModalState();
+
+  const handleFriendSelect = () => {
+    console.log('11');
+    openModal();
+  };
+  return (
+    <>
+      <TopTitle title="일정 생성" />
+      <form className="pt-0">
+        <div className="pb-4 pt-3">
+          <p className="pb-3">일정 제목</p>
+          <input
+            className="bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 w-full"
+            type="text"
+          />
         </div>
-      </div>
-      <div>
-        <WideButton type="submit" text="일정 등록" status={false} />
-      </div>
-    </form>
-  </>
-);
+        <div className="pb-4 pt-3">
+          <p className="pb-3">날짜</p>
+          <SelectDate />
+        </div>
+        <div className="pb-4 pt-3">
+          <p className="pb-3">시간</p>
+          <TimePicker />
+        </div>
+        <div className="pb-4 pt-3">
+          <p className="pb-3">장소</p>
+          <Addr />
+        </div>
+        <div className="pb-4 pt-3">
+          <p className="pb-3">알림 설정</p>
+          <SelectRemind />
+        </div>
+        <div className="pb-4 pt-3">
+          <p className="pb-3">친구선택</p>
+          <div>
+            <button
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+              className="max-w-default m-auto w-full text-body1 h-14 rounded bottom-0 'text-gray4 bg-white border border-dashed border-gray4"
+              type="button"
+              onClick={handleFriendSelect}
+            >
+              친구 선택하기
+              <IoPersonAddOutline style={{ marginLeft: '8px' }} />
+            </button>
+          </div>
+        </div>
+        <div>
+          <WideButton type="submit" text="일정 등록" status={false} />
+        </div>
+      </form>
+      <Modal title="내 친구 목록">
+        <FriendListModal />
+      </Modal>
+    </>
+  );
+};
 
 export default CreateTodoPage;
