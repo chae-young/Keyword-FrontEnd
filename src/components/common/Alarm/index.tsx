58a3f03e2db815a148fa512de793445d8ec26d24
chae-young <cycy8527@gmail.com>
2024-01-10 14:00:05 +0900

    #35 Design: header 컴포넌트

---
+++ src/components/common/Alarm/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaRegBell } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
+
+const Alarm = () => (
+  <Link
+    to="/alarm"
+    className="w-14 h-full flex justify-center items-center absolute right-0 top-0"
+  >
+    <FaRegBell className="text-xl text-bk" />
+  </Link>
+);
+
+export default Alarm;
