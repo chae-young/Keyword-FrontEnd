0ba79b34d2af983e2966458d6441bb77c041ff56
chae-young <cycy8527@gmail.com>
2024-01-11 23:00:25 +0900

    Fix: 알림 시간 옵션 값 수정

--- src/components/common/InnerCon/index.tsx
+++ src/components/common/InnerCon/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const InnerCon = () => (
-  <section className="max-w-default m-auto px-5 pt-20">
+  <section className="max-w-default m-auto px-5 pt-20 pb-16">
     <Outlet />
   </section>
 );
