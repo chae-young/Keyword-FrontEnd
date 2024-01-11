0598e7c3b456a2c907a145eb76b0e1be3f8ae13c
chae-young <cycy8527@gmail.com>
2024-01-11 22:59:38 +0900

    Add: ScheduleCreateDataType

--- src/types/schedule/scheduleDataType.ts
+++ src/types/schedule/scheduleDataType.ts
@@ -1,3 +1,5 @@
+import { FriendsDataType } from '../friend/friendsDataType';
+
 export interface ScheduleDataType {
   scheduleId: number;
   title: string;
@@ -5,3 +7,18 @@ export interface ScheduleDataType {
   locationExplanation: string;
   status: string;
 }
+export interface ScheduleCreateDataType {
+  title: string;
+  contents: string;
+  scheduleDateTime: string;
+  locationExplanation: string;
+  latitude: string;
+  longitude: string;
+  remindDateTime: string;
+  friendList: FriendsDataType[];
+}
+
+export type ScheduleTime = {
+  date: string;
+  time: string;
+};
