d5f69b4b0ab5c8c4ee93e880d704e9b3497d21d2
chae-young <cycy8527@gmail.com>
2024-01-11 22:58:41 +0900

    Add: 카카오 gecoder 추가

--- src/components/common/Map/index.tsx
+++ src/components/common/Map/index.tsx
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DaumPostcode, { Address } from 'react-daum-postcode';
+import useScheduleState from '@/hooks/recoil/useScheduleState';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 const SearchPlace = () => {
+  const { setScheduleCreateState } = useScheduleState();
   const [isOpen, setIsOpen] = useState(false);
   const [address, setAddress] = useState('');
 
@@ -10,26 +18,54 @@ const SearchPlace = () => {
   };
 
   const handleComplete = (data: Address) => {
-    console.log(data);
     onToggleModal();
     setAddress(data.address);
+    setScheduleCreateState(prevSchedule => ({
+      ...prevSchedule,
+      locationExplanation: data.address
+    }));
   };
 
+  useEffect(() => {
+    // 위도 및 경도 좌푯값 구하기
+    const geocoder = new window.kakao.maps.services.Geocoder();
+    geocoder.addressSearch(address, (result: any, status: any) => {
+      if (status === window.kakao.maps.services.Status.OK) {
+        // console.log(`위도 : ${result[0].y}`);
+        // console.log(`경도 : ${result[0].x}`);
+        const lat = `${result[0].x}`;
+        const long = `${result[0].y}`;
+
+        setScheduleCreateState(prevSchedule => ({
+          ...prevSchedule,
+          latitude: lat,
+          longitude: long
+        }));
+      }
+    });
+  }, [address]);
+
   return (
-    <div>
+    <div className="flex">
       <input
-        className="bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 w-80"
+        className="bg-gray2 rounded-lg text-body2 placeholder-text-gray4 h-10 pl-3 w-80 flex-1"
         type="text"
-        placeholder="주소를 검색해보세요"
+        placeholder="주소를 검색해주세요."
         value={address}
         readOnly
       />
-      <button className="btn" type="button" onClick={onToggleModal}>
+      <button
+        className="border border-gray3 w-16 ml-2 rounded-lg text-body2"
+        type="button"
+        onClick={onToggleModal}
+      >
         검색
       </button>
       {isOpen && (
-        <div>
-          <DaumPostcode onComplete={handleComplete} />
+        <div className="before:content-[''] before:fixed before:top-0 before:left-0 before:w-full before:h-full before:bg-black before:opacity-45 before:z-10">
+          <div className="fixed top-1/2 left-0 right-0 m-auto -translate-y-1/2 z-20">
+            <DaumPostcode onComplete={handleComplete} />
+          </div>
         </div>
       )}
     </div>
--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -8,7 +8,7 @@ interface ModalProps {
 
 const Modal = ({ children, title }: ModalProps) => (
   <dialog id="Modal" className="modal -z-10">
-    <div className="modal-box w-11/12 max-w-default">
+    <div className="modal-box w-11/12 max-w-default overflow-hidden">
       <header className="grid grid-cols-[1fr,30px]">
         <h2 className="text-h3 flex items-center">{title && title}</h2>
         <form method="dialog">
