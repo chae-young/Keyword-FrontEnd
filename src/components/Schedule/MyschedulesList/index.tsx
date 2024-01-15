6d78e3168a22d65dd1ccb59584f6aa7b7bc62f97
chae-young <cycy8527@gmail.com>
2024-01-15 18:27:34 +0900

    Fix: #55 데이터 없을경우 에러

--- src/components/Schedule/MyschedulesList/index.tsx
+++ src/components/Schedule/MyschedulesList/index.tsx
@@ -3,26 +3,30 @@ import useGetChatListQuery from '@/hooks/query/chat/useGetChatListQuery';
 import useInfinite from '@/hooks/useInfinite';
 import MySchedulesItem from './MySchdulesItem';
 import useGetSchedulesQuery from '@/hooks/query/schedules/useGetSchedulesQuery';
+import NoDataText from '@/components/common/NoDataText';
 
 const MySchedulesList = () => {
   const { schedulesList, schedulesListFetchNextPage } = useGetSchedulesQuery();
   const { lastElement } = useInfinite(schedulesListFetchNextPage);
-
   return (
     <ul className="[&>*:last-child]:border-0">
       {schedulesList?.pages.map(page =>
-        page.map(list => (
-          <MySchedulesItem
-            key={list.scheduleId}
-            scheduleId={list.scheduleId}
-            title={list.title}
-            scheduleDateTime={list.scheduleDateTime}
-            locationExplanation={list.locationExplanation}
-            status={list.status}
-          />
-        ))
+        page.length ? (
+          page.map(list => (
+            <MySchedulesItem
+              key={list.scheduleId}
+              scheduleId={list.scheduleId}
+              title={list.title}
+              scheduleDateTime={list.scheduleDateTime}
+              locationExplanation={list.locationExplanation}
+              status={list.status}
+            />
+          ))
+        ) : (
+          <NoDataText text="등록한 일정이 없습니다" key={0} />
+        )
       )}
-      {lastElement()}
+      {schedulesList && schedulesList?.pages[0].length >= 10 && lastElement()}
     </ul>
   );
 };
