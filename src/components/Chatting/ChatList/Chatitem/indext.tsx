81cd1f1c18d33608c7821456bfec84a7c398fbdc
Pinn <lsol1130@naver.com>
2024-01-22 19:55:04 +0900

    chatlist UI fix

--- src/components/Chatting/ChatList/Chatitem/indext.tsx
+++ src/components/Chatting/ChatList/Chatitem/indext.tsx
@@ -12,15 +12,24 @@ const ChatItem = ({
   friendsName
 }: ChatItemProps) => {
   console.log(friendsName);
+  const MAX_FRIENDS_TO_DISPLAY = 3;
+
+  console.log(friendsName);
+
+  const displayedFriends = friendsName.slice(0, MAX_FRIENDS_TO_DISPLAY);
+  const additionalFriendsCount = friendsName.length - MAX_FRIENDS_TO_DISPLAY;
 
   return (
     <li className="border-b py-4">
       <Link to={`/chat/${chatRoomId}`}>
         <b>{scheduleTitle}</b>
         <div className="mt-4">
-          {friendsName.map(name => (
+          {displayedFriends.map(name => (
             <span className="text-body2 text-gray4">{name}</span>
           ))}
+          {additionalFriendsCount > 0 && (
+            <span className="text-body2 text-gray4">{`외 ${additionalFriendsCount}명`}</span>
+          )}
         </div>
       </Link>
     </li>
