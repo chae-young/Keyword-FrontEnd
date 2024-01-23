c49faa80ecbd15211dd496d5b68fdc830c7d84c2
chae-young <cycy8527@gmail.com>
2024-01-24 01:26:36 +0900

    #102 Fix: 검색창 깜빡이는 듯한 느낌
    - 스켈레톤으로 융화하고자 했으나,,속도가 넘 빠른듯함

--- src/pages/search/SearchResultPage/index.tsx
+++ src/pages/search/SearchResultPage/index.tsx
@@ -1,7 +1,9 @@
 import { useSearchParams } from 'react-router-dom';
+import { Suspense } from 'react';
 import SearchBar from '@/components/Search/SearchBar';
 import SearchResultList from '@/components/Search/SearchResultList';
 import NoResultText from '@/components/common/NoDataText';
+import UserItem from '@/components/common/Skeleton/UserItem';
 
 const SearchResultPage = () => {
   const [query] = useSearchParams();
